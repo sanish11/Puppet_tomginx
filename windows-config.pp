0821f488defb0890af92f4a83c2888184dec7b83
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-10-02 09:40:45 +0545

    Update windows-config.pp

--- windows-config.pp
+++ windows-config.pp
@@ -10,9 +10,11 @@ node default {
     provider => 'chocolatey',
   }
 
-  # Set JAVA_HOME environment variable using Puppet's environment resource
-  environment { 'JAVA_HOME':
-    value => 'C:\\Program Files\\Java\\jdk1.8.0_291',
+  # Set JAVA_HOME environment variable
+  windows_env { 'JAVA_HOME':
+    ensure    => present,
+    value     => 'C:\Program Files\Java\jdk1.8.0_291',
+    mergemode => 'clobber',
   }
 
   # Install Git via Chocolatey
@@ -24,73 +26,105 @@ node default {
   # Install NVM (Node Version Manager)
   exec { 'install-nvm':
     command => 'Invoke-WebRequest -Uri https://raw.githubusercontent.com/coreybutler/nvm-windows/master/nvm-setup.zip -OutFile C:\nvm-setup.zip; Expand-Archive -Path C:\nvm-setup.zip -DestinationPath C:\nvm; C:\nvm\nvm-setup.exe /SILENT',
-    creates => 'C:\Program Files\nodejs\nvm.exe',
-    path    => 'C:\Windows\System32',
+    creates => 'C:\Program Files\nvm\nvm.exe',
+    provider => 'powershell',
   }
 
   # Install Node.js version 14.17.3 using NVM
   exec { 'install-node':
-    command => 'nvm install 14.17.3 && nvm use 14.17.3',
-    path    => 'C:\Program Files\nvm',
-    require => Package['nvm'],
+    command => 'C:\Program Files\nvm\nvm.exe install 14.17.3 && C:\Program Files\nvm\nvm.exe use 14.17.3',
+    unless  => 'C:\Program Files\nvm\nvm.exe list | Select-String -Pattern "14.17.3"',
+    provider => 'powershell',
+    require => Exec['install-nvm'],
   }
 
   # Install Angular CLI for versions 8, 16, and 20
   exec { 'install-angular-8':
     command => 'npm install -g @angular/cli@8',
-    path    => 'C:\Program Files\nodejs',
+    provider => 'powershell',
     require => Exec['install-node'],
   }
-
   exec { 'install-angular-16':
     command => 'npm install -g @angular/cli@16',
-    path    => 'C:\Program Files\nodejs',
+    provider => 'powershell',
     require => Exec['install-node'],
   }
-
   exec { 'install-angular-20':
     command => 'npm install -g @angular/cli@20',
-    path    => 'C:\Program Files\nodejs',
+    provider => 'powershell',
     require => Exec['install-node'],
   }
 
-  # Set NVM_HOME and NVM_SYMLINK using Puppet's environment resource
-  environment { 'NVM_HOME':
-    value => 'C:\\Program Files\\nvm',
+  # Set NVM_HOME and NVM_SYMLINK
+  windows_env { 'NVM_HOME':
+    ensure    => present,
+    value     => 'C:\Program Files\nvm',
+    mergemode => 'clobber',
+  }
+  windows_env { 'NVM_SYMLINK':
+    ensure    => present,
+    value     => 'C:\Program Files\nodejs',
+    mergemode => 'clobber',
   }
 
-  environment { 'NVM_SYMLINK':
-    value => 'C:\\Program Files\\nodejs',
+  # Add Node.js and NVM to PATH
+  windows_env { 'PATH=C:\Program Files\nodejs;C:\Program Files\nvm':
+    ensure    => present,
+    mergemode => 'append',
   }
 
-  # Add Node.js and NVM to PATH environment variable using Puppet's environment resource
-  environment { 'PATH':
-    value => 'C:\\Program Files\\nodejs;C:\\Program Files\\nvm',
-    append => true, # Ensures the new paths are appended to the existing PATH
-  }
-
-  # Install Maven 3.9.6 using PowerShell for downloading and extraction
+  # Install Maven 3.9.6
   exec { 'download-maven':
-    command => 'powershell.exe -Command "Invoke-WebRequest -Uri https://archive.apache.org/dist/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.zip -OutFile C:\\apache-maven-3.9.6-bin.zip"',
+    command => 'Invoke-WebRequest -Uri https://archive.apache.org/dist/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.zip -OutFile C:\apache-maven-3.9.6-bin.zip',
     creates => 'C:\apache-maven-3.9.6-bin.zip',
-    path    => 'C:\Windows\System32\WindowsPowerShell\v1.0',
+    provider => 'powershell',
   }
-
   exec { 'extract-maven':
-    command => 'powershell.exe -Command "Expand-Archive -Path C:\\apache-maven-3.9.6-bin.zip -DestinationPath C:\\Program Files"',
+    command => 'Expand-Archive -Path C:\apache-maven-3.9.6-bin.zip -DestinationPath "C:\Program Files"',
     creates => 'C:\Program Files\apache-maven-3.9.6',
-    path    => 'C:\Windows\System32\WindowsPowerShell\v1.0',
+    provider => 'powershell',
     require => Exec['download-maven'],
   }
 
-  # Set MAVEN_HOME environment variable using Puppet's environment resource
-  environment { 'MAVEN_HOME':
-    value => 'C:\\Program Files\\apache-maven-3.9.6',
+  # Set MAVEN_HOME and add to PATH
+  windows_env { 'MAVEN_HOME':
+    ensure    => present,
+    value     => 'C:\Program Files\apache-maven-3.9.6',
+    mergemode => 'clobber',
+  }
+  windows_env { 'PATH=C:\Program Files\apache-maven-3.9.6\bin':
+    ensure    => present,
+    mergemode => 'append',
   }
 
-  # Add Maven to PATH environment variable using Puppet's environment resource
-  environment { 'PATH':
-    value => 'C:\\Program Files\\apache-maven-3.9.6\\bin',
-    append => true, # Ensures Maven bin path is appended to the existing PATH
+  # Install IntelliJ IDEA Community Edition
+  package { 'intellijidea-community':
+    ensure   => installed,
+    provider => 'chocolatey',
+  }
+
+  # Install MobaXterm
+  package { 'mobaxterm':
+    ensure   => installed,
+    provider => 'chocolatey',
+  }
+
+  # Install Windows Subsystem for Linux (WSL)
+  exec { 'install-wsl':
+    command  => 'Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux',
+    unless   => 'If ((Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux).State -eq "Enabled") {exit 0} else {exit 1}',
+    provider => 'powershell',
+  }
+
+  # Install Visual Studio Code
+  package { 'vscode':
+    ensure   => installed,
+    provider => 'chocolatey',
+  }
+
+  # Install Notepad++
+  package { 'notepadplusplus':
+    ensure   => installed,
+    provider => 'chocolatey',
   }
 }
